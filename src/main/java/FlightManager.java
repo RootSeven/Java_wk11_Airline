a6212f4c9e7c620f30a7eca9d2e2d3b3132e4b89
RootSeven <mairifairbairn@gmail.com>
2020-05-02 09:59:02 +0100

    Implements Bubble Sort

--- src/main/java/FlightManager.java
+++ src/main/java/FlightManager.java
@@ -1,3 +1,4 @@
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class FlightManager {
@@ -16,6 +17,22 @@ public class FlightManager {
 
     public int remainingBaggageWeight(Flight flight) {
         return ((flight.getPlane().getPlaneType().getTotalWeight())/2) - (this.totalBaggageWeight(flight));
+    }
+
+    public void bubbleSortPassengerSeats(Flight flight){
+        ArrayList<Passenger> passengerList = flight.getBookedPassengers();
+
+        int n = passengerList.size();
+        for ( int i = 0; i < n - 1; i++ ){
+            for ( int j = 0; j < n - i - 1; j++){
+                if ( passengerList.get(j).getSeatNumber() > passengerList.get(j + 1).getSeatNumber() ){
+                    Passenger temp = passengerList.get(j);
+                    passengerList.set(j, passengerList.get(j + 1));
+                    passengerList.set(j + 1, temp);
+                }
+            }
+        }
+
     }
 
 //    public int seatingArrangementBubbleSort(Flight flight){
--- src/test/java/FlightManagerTest.java
+++ src/test/java/FlightManagerTest.java
@@ -11,17 +11,22 @@ public class FlightManagerTest {
     private Flight flight1;
     private Plane plane1;
     private Passenger passenger1;
+    private Passenger passenger2;
+    private Passenger passenger3;
 
     @Before
     public void before(){
         passenger1 = new Passenger("Raul", 1);
+        passenger2 = new Passenger( "Mairi", 1);
+        passenger3 = new Passenger("Josh", 1);
+
         plane1 = new Plane( PlaneType.BOEING747 );
         flight1 = new Flight(plane1, "ED100", "Glasgow", "Edinburgh", "22:00");
         flightManager = new FlightManager();
 
         flight1.bookPassenger(passenger1);
-        flight1.bookPassenger(passenger1);
-        flight1.bookPassenger(passenger1);
+        flight1.bookPassenger(passenger2);
+        flight1.bookPassenger(passenger3);
     }
 
     @Test
@@ -37,6 +42,15 @@ public class FlightManagerTest {
     @Test
     public void canGetRemainingBaggageWeight(){
         assertEquals( 0, flightManager.remainingBaggageWeight(flight1) );
+    }
+
+    @Test
+    public void canBubbleSortPassengerSeats(){
+        flightManager.bubbleSortPassengerSeats(flight1);
+
+        assertEquals( 1, flight1.getBookedPassengers().get(0).getSeatNumber() );
+        assertEquals( 2, flight1.getBookedPassengers().get(1).getSeatNumber() );
+        assertEquals( 3, flight1.getBookedPassengers().get(2).getSeatNumber() );
     }
 
 }
